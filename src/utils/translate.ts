35fbbbbc86ab15c817ba0cb02154fe032f006bd2
lhq01844380@antgroup.com <lhq01844380@antgroup.com>
2024-05-16 10:52:04 +0800

    分片翻译

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,16 +1,36 @@
 import * as core from '@actions/core'
 import GoogleTranslate from '@tomsun28/google-translate-api'
-import { isEnglish } from './isEnglish'
 
 export async function translate(text: string): Promise<string | undefined> {
   try {
-    const resp = await GoogleTranslate(text, {to: 'en'})
-    return resp.text !== text ? resp.text : ''
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const chunks = splitText(text, 1000);
+    core.info("分为几个部分翻译：" +chunks.length);
+    const translatedChunks = [];
+    for (const chunk of chunks) {
+      const resp = await  GoogleTranslate(text, {to: 'en'});
+      translatedChunks.push(resp.text);
+    }
+    return translatedChunks.join('')
   } catch (err: any) {
     core.error(err)
     core.setFailed(err.message)
   }
+}
+
+function splitText(text: string, chunkSize: number) {
+  const chunks = [];
+  let currentChunk = '';
+  for (const char of text) {
+    currentChunk += char;
+    if (currentChunk.length >= chunkSize) {
+      chunks.push(currentChunk);
+      currentChunk = '';
+    }
+  }
+  if (currentChunk) {
+    chunks.push(currentChunk);
+  }
+  return chunks;
 }
 
 const MAGIC_JOIN_STRING = '@@===='
